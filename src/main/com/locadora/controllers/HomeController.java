4934f79e6a2223cd1d8516145a317ad0bbc5aeda
= <=>
2024-09-07 18:16:40 -0300

    Feat: criando classe cliente

---
+++ src/main/com/locadora/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package main.com.locadora.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HomeController {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Bem-vindo à Página Inicial!</h1>");
+    }
+	
+}
---
+++ src/main/com/locadora/models/entities/Cliente.java
@@ -0,0 +1,48 @@
+package main.com.locadora.models.entities;
+
+public class Cliente {
+	String nome;
+	String endereco;
+	String email;
+	String telefone;
+
+	public Cliente(String nome, String endereco, String email, String telefone) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}
