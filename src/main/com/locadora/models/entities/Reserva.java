c64f1c24899088ad176be1f3cc3d1d129a5493b8
BrunoMagno <bmagnoserver@gmail.com>
2024-09-08 16:43:11 -0300

    creation of the reserve class

---
+++ src/main/com/locadora/models/entities/Reserva.java
@@ -0,0 +1,61 @@
+package main.com.locadora.models.entities;
+import java.time.LocalDate;
+
+public class Reserva {
+    private int id;
+    private Cliente cliente;
+    private Veiculo veiculo;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+
+    public Reserva(int id, Cliente cliente, Veiculo veiculo, LocalDate dataInicio, LocalDate dataFim) {
+        this.id = id;
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Veiculo getVeiculo(){
+        return veiculo;
+    }
+    public void setVeiculo(){
+        this.veiculo = veiculo;
+    }
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+    public LocalDate getDataFim(LocalDate dataFim) {
+        return dataFim;
+    }
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public boolean estaDentroDoPeriodo(LocalDate data) {
+        return !data.isBefore(dataInicio) && !data.isAfter(dataFim);
+    }
+
+}
+
+
+
+
+
